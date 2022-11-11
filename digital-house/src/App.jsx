f8525ced315d16576f24be82e6f188c9856feed4
Andrea Matsunaga <andreamizuno@gmail.com>
2022-11-11 00:41:05 -0300

    minor changes

--- digital-house/src/App.jsx
+++ digital-house/src/App.jsx
@@ -4,6 +4,9 @@ import { Lesson04 } from "./lessons/Lesson04"
 import { Lesson05 } from "./lessons/Lesson05"
 import { Lesson07Example } from "./lessons/Lesson07Example"
 import { Lesson07Exercise } from "./lessons/Lesson07Exercise"
+import { Lesson08Rejuvenescedor } from "./lessons/Lesson08Rejuvenescedor"
+import { Lesson13Aula } from "./lessons/Lesson13Aula"
+import { Lesson13Mesa } from "./lessons/Lesson13Mesa"
 import { DHGames } from "./pages/DHGames"
 import { Login } from "./pages/Login"
 
@@ -16,9 +19,12 @@ function App() {
       {/* <Lesson03 /> */}
       {/* <Lesson04 /> */}
       {/* <Lesson05 /> */}
-      <DHGames />
+      {/* <DHGames /> */}
       {/* <Lesson07Example /> */}
       {/* <Lesson07Exercise /> */}
+      {/* <Lesson08Rejuvenescedor /> */}
+      {/* <Lesson13Aula /> */}
+      <Lesson13Mesa />
     </>
   )
 }
